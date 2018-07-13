ee4fee9387c606c29e753154d39dd79b804d27e6
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-13 09:16:10 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n;
+Scanner ja=new Scanner (System.in);
+n=ja.nextInt();
+if(n%2==0){
+	System.out.println("even");
+}
+else{
+	System.out.println("odd");
+}
+	}
+
+}
